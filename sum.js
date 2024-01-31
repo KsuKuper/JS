c88aadf306dcccbf8f1e965706f7577475f95666
KsuKuper <nekrashevich.k@mail.ru>
2024-01-31 17:10:35 +0300

    added the JEST framework and added unit tests to the calculateBonus function as required in hw_ 3.1.1

--- sum.js
+++ sum.js
@@ -1,9 +1,16 @@
 const calculateBonus = (a, b) => {
-    let bonus;
-    const sum = a + b;
-    debugger;
-    sum > 50 ? (bonus = 50) : (bonus = sum);
-    debugger;
-    return bonus;
-  };
-  console.log(calculateBonus(20, 40));
+  let bonus;
+  const sum = a + b;
+  debugger;
+
+  if (isNaN(sum) || a === "" || b === "") {
+    return 0;
+  }
+  sum > 50 ? (bonus = 50) : (bonus = sum);
+  debugger;
+
+  return bonus;
+};
+console.log(calculateBonus(20, 40));
+
+module.exports = calculateBonus;
